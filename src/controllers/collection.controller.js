ed61180fdebc9f58e4ce42271d7045033db03dd9
anhthunguyenngoc <anhthunguyenngoc80@gmail.com>
2026-07-02 21:30:45 +0700

    add res data

--- src/controllers/collection.controller.js
+++ src/controllers/collection.controller.js
@@ -3,8 +3,8 @@ const collectionModel = require("../model/collection.model");
 const createCollection = async (req, res) => {
   const { user_id, name } = req.body;
   try {
-    const result = collectionModel.createCollection(user_id, name);
-    res.status(201).json({ message: "Create successfull" });
+    const result = await collectionModel.createCollection(user_id, name);
+    res.status(201).json({ message: "Create successfull", data: result });
   } catch (error) {
     console.log("Create collection failed", error);
     res.status(401).json({ message: "Create failed" });
@@ -14,8 +14,10 @@ const createCollection = async (req, res) => {
 const getAllCollections = async (req, res) => {
   const { user_id } = req.body;
   try {
-    const result = collectionModel.getAllCollections(user_id);
-    res.status(201).json({ message: "Get collections successfully" });
+    const result = await collectionModel.getAllCollections(user_id);
+    res
+      .status(201)
+      .json({ message: "Get collections successfully", data: result });
   } catch (error) {
     console.log("Query failed", error);
     res.status(401).json({ message: "Query failed" });
@@ -26,8 +28,13 @@ const getCollectionById = async (req, res) => {
   const { user_id } = req.body;
   const { collectionId } = req.params;
   try {
-    const result = collectionModel.getCollectionById(user_id, collectionId);
-    res.status(201).json({ message: "Get collection successfully" });
+    const result = await collectionModel.getCollectionById(
+      user_id,
+      collectionId,
+    );
+    res
+      .status(201)
+      .json({ message: "Get collection successfully", data: result });
   } catch (error) {
     console.log("Query failed", error);
     res.status(401).json({ message: "Query failed" });
